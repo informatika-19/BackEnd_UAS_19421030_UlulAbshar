b8f07847de35889d293691a2e9fba10c470b42cc
ululabshar <ululabshar10566@gmail.com>
2021-07-14 00:07:22 +0700

    first commit

---
+++ model/Barang.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const BarangSchema = new Schema({
+    namaBarang: {
+        type: String
+    },
+    harga: {
+        type: Number
+    },
+    typeBarang: {
+        type: String
+    },
+    deskripsi: {
+        type: String
+    },
+    image: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model('Barang', BarangSchema)
